d9b0cf0eb9796a58dd83f0458350d6c2c465c0fa
Samir Morhill <samir7morhill@gmail.com>
2025-07-17 11:10:37 -0300

    entidad producto creada

---
+++ Dsw2025Tpi.Api/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2025Tpi.Api.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Dsw2025Tpi.Domain/Entities/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Domain.Entities
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(string sku, string internalCode, string name, string description, decimal price, decimal stockQuantity)
+        {
+            Sku = sku;
+            InternalCode = internalCode;
+            Name = name;
+            Description = description;
+            CurrentUnitPrice = price;
+            StockQuantity = (int)stockQuantity;
+            IsActive = true;
+            ProductId = Guid.NewGuid();
+        }
+
+        public string? Sku { get; set; }
+        public string? InternalCode { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal CurrentUnitPrice { get; set; }
+        public int StockQuantity { get; set; }
+        public bool IsActive { get; set; }
+        public Guid ProductId { get; private set; }
+        //public ICollection<OrderItem> OrderItems { get; set; } = new HashSet<OrderItem>();
+
+
+
+
+    }
+}
